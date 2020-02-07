23cc794f3ffe9e7d6c0127de97c68a425fff215a
Philip <p208p2002@gmail.com>
2020-02-07 09:28:32 +0800

    no message

--- app/UserAuthLimit.php
+++ app/UserAuthLimit.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserAuthLimit extends Model
 {
     //
+    protected $primaryKey = ['uid','app_id'];
     public $incrementing = false;
     public function appKeyManage(){
         return $this->belongsTo('App\AppKeyManage','app_id','id');
